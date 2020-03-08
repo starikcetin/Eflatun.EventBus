81ff14a7cd1998341a890d7ce2c6bdd66c8182c0
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2020-03-08 16:07:27 +0300

    improvements
    1. Remove listener
    2. Ability to listen before/after the event.

--- Eflatun.EventBus/Packages/com.eflatun.eventbus/src/EventBus.cs
+++ Eflatun.EventBus/Packages/com.eflatun.eventbus/src/EventBus.cs
@@ -5,16 +5,47 @@ namespace Eflatun.EventBus
     public class EventBus<TEvent>
         where TEvent : IEvent
     {
+        private readonly List<EventHandler<TEvent>> _listenersBefore = new List<EventHandler<TEvent>>();
         private readonly List<EventHandler<TEvent>> _listeners = new List<EventHandler<TEvent>>();
+        private readonly List<EventHandler<TEvent>> _listenersAfter = new List<EventHandler<TEvent>>();
 
         public void Emit(object sender, TEvent @event)
         {
+            _listenersBefore.ForEach(x => x?.Invoke(sender, @event));
             _listeners.ForEach(x => x?.Invoke(sender, @event));
+            _listenersAfter.ForEach(x => x?.Invoke(sender, @event));
         }
 
-        public void Listen(EventHandler<TEvent> listener)
+        /// <summary>
+        /// Add a listener that is invoked before regular listeners.
+        /// </summary>
+        public void AddListenerBefore(EventHandler<TEvent> listener)
+        {
+            _listenersBefore.Add(listener);
+        }
+
+        /// <summary>
+        /// Add a regular listener.
+        /// </summary>
+        public void AddListener(EventHandler<TEvent> listener)
         {
             _listeners.Add(listener);
+        }
+
+        /// <summary>
+        /// Add a listener that is invoked after the regular listeners.
+        /// </summary>
+        public void AddListenerAfter(EventHandler<TEvent> listener)
+        {
+            _listenersAfter.Add(listener);
+        }
+
+        /// <summary>
+        /// Remove a listener (from before, regular, and active lists).
+        /// </summary>
+        public void RemoveListener(EventHandler<TEvent> listener)
+        {
+            _listeners.Remove(listener);
         }
     }
 }
